b6ef1bf6ee1ab10643b17c8ea6cddd588eb46e92
Seednode <git@seedno.de>
2022-05-11 17:35:32 -0500

    Reformatted file

--- cmd/logging.go
+++ cmd/logging.go
@@ -12,33 +12,43 @@ func logOutput(logFile string) func() {
 	_, err := os.Stat(logFile)
 	if errors.Is(err, os.ErrNotExist) == false {
 		fmt.Print("File " + logFile + " already exists. Remove? (y/N) ")
+
 		input := bufio.NewScanner(os.Stdin)
 		input.Scan()
+
 		if input.Text() != "y" {
 			os.Exit(1)
 		}
+
 		err := os.Remove(logFile)
 		if err != nil {
 			panic(err)
 		}
 	}
+
 	logFilePtr, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
 		panic(err)
 	}
+
 	out := os.Stdout
+
 	multiWriter := io.MultiWriter(out, logFilePtr)
+
 	reader, writer, err := os.Pipe()
 	if err != nil {
 		panic(err)
 	}
+
 	os.Stdout = writer
 	os.Stderr = writer
+
 	exit := make(chan bool)
 	go func() {
 		_, _ = io.Copy(multiWriter, reader)
 		exit <- true
 	}()
+
 	return func() {
 		_ = writer.Close()
 		<-exit
