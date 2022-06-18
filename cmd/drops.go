b060d0f6b318d639b06bc4b5759180aaefe65a34
Seednode <git@seedno.de>
2022-06-18 14:03:36 -0500

    Added initial boilerplate for parsing dropped packets from logs

--- cmd/drops.go
+++ cmd/drops.go
@@ -1,1 +1,25 @@
+/*
+Copyright © 2022 Seednode <seednode@seedno.de>
+*/
+
 package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func DroppedPackets(arguments []string) {
+}
+
+var droppedCmd = &cobra.Command{
+	Use:   "dropped <file1> [file2]...",
+	Short: "Parse out timestamps of drops from log file(s)",
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		DroppedPackets(args)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(stripCmd)
+}
